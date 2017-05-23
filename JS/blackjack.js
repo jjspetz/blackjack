9ce88df3768c88f478e4a1ab1e84174ea97e4f80
JJ <jj.spetseris@yahoo.com>
2017-05-23 09:05:30 -0500

    update before deployment

---
+++ JS/blackjack.js
@@ -0,0 +1,58 @@
+
+// an array of possible cards
+var deck = [
+  { label: 2, suit: 'clubs', img: '2oC.png' };
+  { label: 2, suit: 'spades', img: '2oS.png' };
+  { label: 2, suit: 'dimonds', img: '2oD.png' };
+  { label: 2, suit: 'hearts', img: '2oH.png' };
+  { label: 3, suit: 'clubs', img: '3oC.png' };
+  { label: 3, suit: 'spades', img: '3oS.png' };
+  { label: 3, suit: 'dimonds', img: '3oD.png' };
+  { label: 3, suit: 'hearts', img: '3oH.png' };
+  { label: 4, suit: 'clubs', img: '4oC.png' };
+  { label: 4, suit: 'spades', img: '4oS.png' };
+  { label: 4, suit: 'dimonds', img: '4oD.png' };
+  { label: 4, suit: 'hearts', img: '4oH.png' };
+  { label: 5, suit: 'clubs', img: '5oC.png' };
+  { label: 5, suit: 'spades', img: '5oS.png' };
+  { label: 5, suit: 'dimonds', img: '5oD.png' };
+  { label: 5, suit: 'hearts', img: '5oH.png' };
+  { label: 6, suit: 'clubs', img: '6oC.png' };
+  { label: 6, suit: 'spades', img: '6oS.png' };
+  { label: 6, suit: 'dimonds', img: '6oD.png' };
+  { label: 6, suit: 'hearts', img: '6oH.png' };
+  { label: 7, suit: 'clubs', img: '7oC.png' };
+  { label: 7, suit: 'spades', img: '7oS.png' };
+  { label: 7, suit: 'dimonds', img: '7oD.png' };
+  { label: 7, suit: 'hearts', img: '7oH.png' };
+  { label: 8, suit: 'clubs', img: '8oC.png' };
+  { label: 8, suit: 'spades', img: '8oS.png' };
+  { label: 8, suit: 'dimonds', img: '8oD.png' };
+  { label: 8, suit: 'hearts', img: '8oH.png' };
+  { label: 9, suit: 'clubs', img: '9oC.png' };
+  { label: 9, suit: 'spades', img: '9oS.png' };
+  { label: 9, suit: 'dimonds', img: '9oD.png' };
+  { label: 9, suit: 'hearts', img: '9oH.png' };
+  { label: 10, suit: 'clubs', img: '10oC.png' };
+  { label: 10, suit: 'spades', img: '10oS.png' };
+  { label: 10, suit: 'dimonds', img: '10oD.png' };
+  { label: 10, suit: 'hearts', img: '10oH.png' };
+  { label: 'J', suit: 'clubs', img: 'JoC.png' };
+  { label: 'J', suit: 'spades', img: 'JoS.png' };
+  { label: 'J', suit: 'dimonds', img: 'JoD.png' };
+  { label: 'J', suit: 'hearts', img: 'JoH.png' };
+  { label: 'Q', suit: 'clubs', img: 'QoC.png' };
+  { label: 'Q', suit: 'spades', img: 'QoS.png' };
+  { label: 'Q', suit: 'dimonds', img: 'QoD.png' };
+  { label: 'Q', suit: 'hearts', img: 'QoH.png' };
+  { label: 'K', suit: 'clubs', img: 'KoC.png' };
+  { label: 'K', suit: 'spades', img: 'KoS.png' };
+  { label: 'K', suit: 'dimonds', img: 'KoD.png' };
+  { label: 'K', suit: 'hearts', img: 'KoH.png' };
+  { label: 'A', suit: 'clubs', img: 'AoC.png' };
+  { label: 'A', suit: 'spades', img: 'AoS.png' };
+  { label: 'A', suit: 'dimonds', img: 'AoD.png' };
+  { label: 'A', suit: 'hearts', img: 'AoH.png' };
+];
+// the array of cards to be dealt
+var cards = []
