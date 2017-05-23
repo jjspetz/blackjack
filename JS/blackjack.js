6b0a5ff9c2c557f3f542fc12cef3024665287e2a
JJ <jj.spetseris@yahoo.com>
2017-05-23 15:42:20 -0500

    basics work

--- JS/blackjack.js
+++ JS/blackjack.js
@@ -1,3 +1,7 @@
+// global variables
+var deck;
+var shuffled_deck;
+
 // deck building function
 var deckArray = []
 function buildDeck() {
@@ -10,14 +14,11 @@ function buildDeck() {
   });
   return deckArray;
 }
+
 // shuffle the deck
-var deck = buildDeck();
-console.log(deck);
-var shuffled_deck = [];
 function shuffle() {
   var index = Math.floor(Math.random() * deck.length)
   var card = deck[index];
-  console.log(card);
   deck.splice(index, 1);
   shuffled_deck.push(card);
   if (deck.length > 0) {
@@ -26,24 +27,85 @@ function shuffle() {
 }
 
 // set up for new game
-shuffle();
+function game() {
+  deck = buildDeck();
+  shuffled_deck = [];
+  // shuffle the new deck
+  shuffle();
 
-// the array of cards dealt
-var dealtCards = []
+  // reset windows
+  $('.hand').html('');
+  $('#messages').html('');
+  // reset points
+  $('#dealer-points').html('0');
+  $('#player-points').html('0');
+  // reset buttons
+  $('#hit-button').prop('disabled', false);
+  $('#stand-button').prop('disabled', false);
+  $('#restart-button').prop('disabled', true);
+
+  // initial deals
+  $('#dealer-hand').append('<img src="images/back.png">');
+  give('dealer');
+  give('player');
+  give('player');
+}
+// runs game on load
+game();
 
 // deals random card
 function give(player) {
-      var next = shuffled_deck.pop();
-      $('#'+player+'-hand').append('<img src="images/'+ next.img + '">');
+  // pops top card from the deck and assigns to variable for function use
+  var next = shuffled_deck.pop();
+  // calls calc function on top card to add to the player's total
+  $('#'+player+'-points').html( parseInt($('#'+player+'-points').html()) + calc(next));
+  check(player);
+  $('#'+player+'-hand').append('<img src="images/'+ next.img + '">');
 }
 
-// deal function
-$('#deal-button').click(function() {
-  give('dealer');
-  give('player');
+// calculates the value of the card
+function calc(card) {
+  var val = parseInt(card.label);
+  if (isNaN(val)) {
+    if (card.label == 'ace') {
+      val = 11;
+    } else {
+      val = 10;
+    }
+  }
+  return val;
+}
+
+// checks for bust and displays msg if bust as well as disables buttons
+function check(player) {
+  if ($('#'+player+'-points').html() > 21) {
+    $('#messages').html("<h2>" + player + " busts!</h2>")
+    $('#hit-button').prop('disabled', true);
+    $('#stand-button').prop('disabled', true);
+    $('#restart-button').prop('disabled', false);
+  }
+}
+
+// stand function
+$('#stand-button').click(function() {
+  $('#dealer-hand img:first-of-type').hide();
+  var next = shuffled_deck.pop();
+  // calls calc function on top card to add to the player's total
+  $('#dealer-points').html( parseInt($('#dealer-points').html()) + calc(next));
+  check('dealer');
+  $('#dealer-hand').prepend('<img src="images/'+ next.img + '">');
+  // dealer hits if under 17
+  while ( $('#dealer-points').html() < 17) {
+    give('dealer');
+  }
 });
 
 // hit function
 $('#hit-button').click(function() {
   give('player');
+});
+
+// restart game function
+$('#restart-button').click(function() {
+  game();
 });
